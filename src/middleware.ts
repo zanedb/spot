ee230ea2e5bfc263641b986dc6868857e2f96061
Zane <github@zane.sh>
2024-02-29 17:43:53 -0800

    Lodash is silly

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { isEmpty } from 'lodash'
 import * as jose from 'jose'
-
 const { JWT_SECRET } = process.env
 
 export async function middleware(request: NextRequest) {
   // Basic validation
   const authorization = request.headers.get('Authorization')
-  if (!authorization || isEmpty(authorization))
+  if (!authorization || !(authorization.length > 0))
     return NextResponse.json(
       { error: 'missing authorization' },
       { status: 401 }
